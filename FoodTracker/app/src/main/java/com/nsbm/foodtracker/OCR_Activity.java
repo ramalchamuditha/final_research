c8db24ff9407ce7dd5df7d7e9ac228f39618c98b
Ramal Chamuditha <36332204+ramalchamuditha@users.noreply.github.com>
2022-02-09 06:09:12 +0530

    OCR-changes1

--- FoodTracker/app/src/main/java/com/nsbm/foodtracker/OCR_Activity.java
+++ FoodTracker/app/src/main/java/com/nsbm/foodtracker/OCR_Activity.java
@@ -1,24 +1,35 @@
 package com.nsbm.foodtracker;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.camera.core.ImageAnalysis;
+import androidx.camera.core.ImageProxy;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 
 import android.Manifest;
+import android.annotation.SuppressLint;
 import android.content.pm.PackageManager;
+import android.media.Image;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.mlkit.vision.common.InputImage;
+import com.google.mlkit.vision.text.Text;
 import com.google.mlkit.vision.text.TextRecognition;
 import com.google.mlkit.vision.text.TextRecognizer;
 import com.google.mlkit.vision.text.latin.TextRecognizerOptions;
 
-public class OCR_Activity extends AppCompatActivity {
+public class OCR_Activity extends AppCompatActivity implements ImageAnalysis.Analyzer{
 
     Button btnCapture,btncopy;
     TextView viewText;
+    String result = null;
     private static final int REQUESTS_CAMERA_CODE = 100;
 
     @Override
@@ -41,10 +52,34 @@ public class OCR_Activity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
 
-                
             }
         });
 
 
     }
+
+    @Override
+    public void analyze(@NonNull ImageProxy imageProxy) {
+        Image mediaImage = imageProxy.getImage();
+        if (mediaImage != null)
+        {
+            InputImage image = InputImage.fromMediaImage(mediaImage, imageProxy.getImageInfo().getRotationDegrees());
+            TextRecognizer recognizer = TextRecognition.getClient(TextRecognizerOptions.DEFAULT_OPTIONS);
+            Task<Text>  result = recognizer.process(image)
+                    .addOnSuccessListener(new OnSuccessListener<Text>() {
+                        @Override
+                        public void onSuccess(Text text) {
+
+                        }
+                    })
+                    .addOnFailureListener(new OnFailureListener() {
+                        @Override
+                        public void onFailure(@NonNull Exception e) {
+
+                        }
+                    });
+        }
+
+    }
+
 }
